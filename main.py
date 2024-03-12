bf48ca9b8ca433aa5caa5946e1e94a8e6f488f5a
Michael Quach <90778766+mq089@users.noreply.github.com>
2024-03-12 17:33:57 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#Group 41
+#Michael Quach: 300177284
+#Aleana Wright: 300196531
+#Hamza Israr: 300168510
+
+#Currently there isn't anything here... this is just a test file so I can upload it to the github -Michael
+
+
+
+import sqlite3
