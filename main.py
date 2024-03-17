72174dda0f6456ecfb116ee6ebc8e0c3ed193e2b
Michael Quach <mquac089@uottawa.ca>
2024-03-17 15:24:34 -0400

    made some updates

--- main.py
+++ main.py
@@ -3,8 +3,69 @@
 #Aleana Wright: 300196531
 #Hamza Israr: 300168510
 
-#Currently there isn't anything here... this is just a test file so I can upload it to the github -Michael
+import psycopg2
+import pandas as pd
+
+#connect to database remotely
+conn = psycopg2.connect(dbname='railway', user='postgres', password='EYndBSqLdilpjZfzdWFAOiNTdRrozMko', host='viaduct.proxy.rlwy.net', port='41102', sslmode='require')
+
+cursor = conn.cursor()
+
+
+#------------------Create all the tables here--------------------------
+
+#Create Time Dimension table if it doesn't exist
+sql = """ 
+CREATE TABLE IF NOT EXISTS time_dimension (  
+    id SERIAL PRIMARY KEY, 
+    year INT NOT NULL
+);
+""" 
+cursor.execute(sql)
+conn.commit()
+
+#create Geography Dimension if it doesn't exist
+sql = """ 
+CREATE TABLE IF NOT EXISTS geography_dimension (  
+    geography_key SERIAL PRIMARY KEY, 
+    country TEXT,
+    state TEXT
+);
+""" 
+cursor.execute(sql)
+conn.commit()
+
+#create demopgrahic dimension if it doesn't exist
+sql = """ 
+CREATE TABLE IF NOT EXISTS demopgrahic_dimension (  
+    demographic_key SERIAL PRIMARY KEY, 
+    sex TEXT,
+    age_range TEXT
+);
+""" 
+cursor.execute(sql)
+conn.commit()
+
+#create death dimension if it doesn't exist
+sql = """ 
+CREATE TABLE IF NOT EXISTS death_dimension (  
+    death_key SERIAL PRIMARY KEY, 
+    death_desription TEXT,
+    mortality_id TEXT
+);
+""" 
+
+#create cause table if it doesn't exist
+#table to hold ids ofr ICD-10 causes of death
+sql = """ 
+CREATE TABLE IF NOT EXISTS cause_table (  
+    code_key SERIAL PRIMARY KEY, 
+    description TEXT
+);
+""" 
 
 
 
-import sqlite3
+
+
+
