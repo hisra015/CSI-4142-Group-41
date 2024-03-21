0d314568022f313deef8a67f24e2e82bdc41c626
Michael Quach <mquac089@uottawa.ca>
2024-03-21 13:51:15 -0400

    uploaded load.py

---
+++ load.py
@@ -0,0 +1,14 @@
+#Group 41
+#Michael Quach: 300177284
+#Aleana Wright: 300196531
+#Hamza Israr: 300168510
+
+import psycopg2
+import pandas as pd
+
+#connect to database remotely
+conn = psycopg2.connect(dbname='railway', user='postgres', password='EYndBSqLdilpjZfzdWFAOiNTdRrozMko', host='viaduct.proxy.rlwy.net', port='41102', sslmode='require')
+
+cursor = conn.cursor()
+
+#This file loads all the data into the PostGres database
