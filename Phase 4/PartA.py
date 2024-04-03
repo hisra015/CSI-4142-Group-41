838a4a275e8bfe5e9f8bb631f3ab888c2bb2cfb3
Michael Quach <mquac089@uottawa.ca>
2024-04-03 16:01:59 -0400

    uploaded files for phase 4

---
+++ Phase 4/PartA.py
@@ -0,0 +1,6 @@
+#Group 41
+#Michael Quach: 300177284
+#Aleana Wright: 300196531
+#Hamza Israr: 300168510
+
+import pandas as pd
---
+++ Phase 4/PartB.py
@@ -0,0 +1,11 @@
+#Group 41
+#Michael Quach: 300177284
+#Aleana Wright: 300196531
+#Hamza Israr: 300168510
+
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.metrics import accuracy_score
