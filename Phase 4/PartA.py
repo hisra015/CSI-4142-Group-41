6450e892d2d56bdcc4dd64415e1eb6bebe2ac151
Michael Quach <mquac089@uottawa.ca>
2024-04-09 15:53:08 -0400

    update

--- Phase 4/PartA.py
+++ Phase 4/PartA.py
@@ -45,7 +45,18 @@ def feature_selection(data):
     return selected_data
 
 def main():
-   #retrieve data from csv
+    #insert data summarization code below
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    #retrieve data from csv
     data = pd.read_csv("Staged_data.csv", low_memory=False)
     data = data.drop(columns='surrogate') #drop surrogate keys as they are uncessary
     
