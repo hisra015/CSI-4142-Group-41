21fbbcff26d714aed25943a4abbb0bdef15bf390
Hamza Israr <hisra015@uottawa.ca>
2024-04-09 21:36:02 -0400

    fixed data summarization

--- Phase 4/DataSummarization.py
+++ Phase 4/DataSummarization.py
@@ -7,12 +7,32 @@ import numpy as np
 data = pd.read_csv('Staged_data.csv')
 path = "Phase 4/Graphs/"
 # Adjusting histogram bins and figure size for better visibility and saving them
-# Surrogate Distribution
-plt.figure(figsize=(8, 6))
-sns.histplot(data['surrogate'], bins=20)
-plt.title('Surrogate Distribution')
-plt.savefig(path+'surrogate_distribution.png')  # Save the figure
-plt.close()  # Close the plot to avoid displaying it in the notebook
+
+# Trend of total deaths over the years
+plt.figure(figsize=(12, 6))
+sns.lineplot(data=data, x='year', y='death_number', estimator='sum', ci=None, marker='o')
+plt.xticks(data['year'].unique(), rotation=45)  # Ensure all years are shown and rotate labels for better visibility
+plt.title('Total Deaths Over Years')
+plt.ylabel('Total Deaths')
+plt.xlabel('Year')
+plt.tight_layout()  # Adjust the layout to make room for the x-axis labels
+plt.savefig(path+'total_deaths_over_years.png')
+plt.close()
+
+# Top causes of death
+# First, let's group the data by 'death_description' and sum the 'death_number'
+cause_death_counts = data.groupby('death_description')['death_number'].sum().reset_index()
+top_causes = cause_death_counts.sort_values(by='death_number', ascending=False).head(10)
+
+plt.figure(figsize=(12, 8))
+sns.barplot(data=top_causes, x='death_number', y='death_description', palette='viridis')
+plt.title('Top Causes of Death')
+plt.xlabel('Number of Deaths')
+plt.ylabel('Cause of Death')
+plt.tight_layout()
+plt.savefig(path+'top_causes_of_death.png')
+plt.close()
+
 
 # Year Distribution
 plt.figure(figsize=(8, 6))
@@ -47,13 +67,6 @@ plt.figure(figsize=(10, 8))
 sns.histplot(data['death_rank'], bins=40, kde=True)
 plt.title('Death Rank Distribution')
 plt.savefig(path+'death_rank_distribution.png')
-plt.close()
-
-# Age Mortality Boxplot
-plt.figure(figsize=(8, 6))
-sns.boxplot(data=data, x='age_mortality')
-plt.title('Age Mortality Boxplot')
-plt.savefig(path+'age_mortality_boxplot.png')
 plt.close()
 
 # Log-transformed Death Number Distribution
